ebca83de05d7b6927b90a04f785c4ee774d1fe1e
Yolol100 <Andrew_Baeten@hotmail.com>
2026-09-04 23:27:58 +0200

    Export observation canonicalization API

--- src/vacature_engine/__init__.py
+++ src/vacature_engine/__init__.py
@@ -1,3 +1,9 @@
+from .observations import (
+    OBSERVATION_CONTRACT_VERSION,
+    canonicalize_observations,
+    normalize_canonical_url,
+    observation_identity_keys,
+)
 from .simple import (
     LOGIC_VERSION,
     VacancyPolicy,
@@ -10,9 +16,13 @@ from .simple import (
 
 __all__ = [
     "LOGIC_VERSION",
+    "OBSERVATION_CONTRACT_VERSION",
     "VacancyPolicy",
+    "canonicalize_observations",
     "choose_language",
     "eligibility",
+    "normalize_canonical_url",
+    "observation_identity_keys",
     "policy_from_config",
     "score",
     "top_vacancies",
