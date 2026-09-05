cf0dbb4c09086a62aebda642e213b35791deadb3
Yolol100 <Andrew_Baeten@hotmail.com>
2026-09-05 15:36:51 +0200

    feat: register Workable adapter

--- ingestion/src/vacature_ingestion/adapters/__init__.py
+++ ingestion/src/vacature_ingestion/adapters/__init__.py
@@ -7,6 +7,7 @@ from .lever import LeverAdapter
 from .personio import PersonioAdapter
 from .remotive import RemotiveAdapter
 from .smartrecruiters import SmartRecruitersAdapter
+from .workable import WorkableAdapter
 
 ADAPTERS = {
     "ashby": AshbyAdapter(),
@@ -18,6 +19,7 @@ ADAPTERS = {
     "personio": PersonioAdapter(),
     "remotive": RemotiveAdapter(),
     "smartrecruiters": SmartRecruitersAdapter(),
+    "workable": WorkableAdapter(),
 }
 
 __all__ = ["ADAPTERS"]
