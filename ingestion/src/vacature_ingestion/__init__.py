c46161291be98ab17539bed027cdf5655b4c406c
Yolol100 <Andrew_Baeten@hotmail.com>
2026-09-05 17:12:55 +0200

    Align ingestion runtime version to 0.8.0

--- ingestion/src/vacature_ingestion/__init__.py
+++ ingestion/src/vacature_ingestion/__init__.py
@@ -4,4 +4,4 @@ from .models import OBSERVATION_CONTRACT_VERSION, SourceSpec
 from .runner import IngestionRunner
 
 __all__ = ["OBSERVATION_CONTRACT_VERSION", "SourceSpec", "IngestionRunner"]
-__version__ = "0.7.0"
+__version__ = "0.8.0"
